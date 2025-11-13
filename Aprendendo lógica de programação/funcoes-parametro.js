59a38379af086a64380e27f8353b41f8aa2f6f7e
brunofreire668 <brunofreire466@gmail.com>
2025-11-12 21:23:49 -0300

    Add files via upload

---
+++ Aprendendo lógica de programação/funcoes-parametro.js
@@ -0,0 +1,39 @@
+
+
+let button = document.querySelector("#btn-somar")
+
+
+
+
+/* let somaValores = soma(5,3)
+
+console.log(soma(5,3))
+console.log(soma(10, 3))
+console.log(soma(5,23)) */
+
+const btnSomar = document.querySelector("#somar")
+
+function somar() {
+    let primeiroValor = Number(prompt("Digite o primeiro número:"))
+    let segundoValor = Number(prompt("Digite o segundo número:"))
+
+    let soma = primeiroValor + segundoValor
+
+  return alert(soma)
+}
+
+button.addEventListener("click", somar)
+
+
+
+
+const btnSoma = document.querySelector("#somar")
+const btnSubtracao = document.querySelector("$sobtracao")
+const btnMultiplicacao = document.querySelector("#multiplicacao")
+const btnDivisao = document.querySelector("#divisao")
+const inputPrimeiroValor = document.querySelector("#primeiroNumero")
+const inputSegundoValor = document.querySelector("#segundoNumero")
+
+function somar() {
+    let 
+}
